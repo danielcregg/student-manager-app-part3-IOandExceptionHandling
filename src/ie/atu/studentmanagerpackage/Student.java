d9f612395b30797f886c298627a23e7712e1abef
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-11-13 21:33:51 +0000

    Update Student.java

--- src/ie/atu/studentmanagerpackage/Student.java
+++ src/ie/atu/studentmanagerpackage/Student.java
@@ -1,35 +1,26 @@
-package ie.atu.studentmanagerpackage;
+package ie.gmit.studentmanagerpackage;
 
 public class Student {
 
 	// Instance Variables
 	private String studentId;
-	private String firstname;
+	private String firstName;
 	private int age;
 
 	// Constructors
-	public Student() {
-		firstname = "null";
-		age = 0;
-	}
-	
-	public Student(String studentId) {
-		this.studentId = studentId;
-	}
-
 	public Student(String studentId, String name, int age) {
 		this.studentId = studentId;
-		this.firstname = name;
+		this.firstName = name;
 		this.age = age;
 	}
-	
+
 	// Getters and Setters
 	public String getStudentId() {
 		return studentId;
 	}
-	
-	public String getFirstname() {
-		return this.firstname;
+
+	public String getFirstName() {
+		return this.firstName;
 	}
 
 	public int getAge() {
@@ -37,20 +28,71 @@ public class Student {
 	}
 
 	public void setStudentId(String studentId) {
-		this.studentId = studentId;
+		if (studentIdIsValid(studentId)) {
+			this.studentId = studentId;
+		}
 	}
 
-	public void setName(String name) {
-		this.firstname = name;
+	public void setFirstName(String name) {
+		if (firstNameIsValid(name)) {
+			this.firstName = name;
+		}
 	}
 
 	public void setAge(int age) {
-		this.age = age;
+		if (ageIsValid(age)) {
+			this.age = age;
+		}
+	}
+
+	// Check if student ID is valid
+	public static boolean studentIdIsValid(String studentId) {
+		// Check if student ID is valid
+		if (studentId == null) {
+			System.err.println("Student ID can not be null");
+			return false;
+		} else if (!(studentId.matches("G00\\d{6}"))) {
+			System.err.println("Student ID " + studentId + " does not match the format G00123456");
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Check if student ID is valid
+	public static boolean firstNameIsValid(String firstName) {
+		if (firstName == null) {
+			System.err.println("First name can not be null");
+			return false;
+		} else if (firstName.length() < 2) {
+			System.err.println("First name must be at least 2 characters long");
+			return false;
+		} else if (!firstName.matches("[a-zA-Z]+")) {
+			System.err.println("First name must conatin Upper and lover case letters only");
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Check if student ID is valid
+	public static boolean ageIsValid(int age) {
+		if (age < 16 || age > 130) {
+			System.err.println("Student must be at least 16 years old and less than 130 years old");
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Check if student details are valid
+	public static boolean isValid(String studentId, String firstName, int age) {
+		return studentIdIsValid(studentId) && firstNameIsValid(firstName) && ageIsValid(age);
 	}
 
 	// Method to print students name and age
+	@Override
 	public String toString() {
-		return this.studentId + "," + this.firstname + "," + this.age;
+		return this.studentId + "," + this.firstName + "," + this.age;
 	}
-
-} // End class
+}
