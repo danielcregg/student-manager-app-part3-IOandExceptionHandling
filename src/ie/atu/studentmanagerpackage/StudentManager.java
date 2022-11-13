6348ff68814110e56159d32723e0d17035c6cd8b
Daniel Cregg <danielcregg@users.noreply.github.com>
2022-11-13 21:32:48 +0000

    Update StudentManager.java

--- src/ie/atu/studentmanagerpackage/StudentManager.java
+++ src/ie/atu/studentmanagerpackage/StudentManager.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class StudentManager {
 
-	// Instance Variables
+// Instance Variables
 	private List<Student> studentList;
 
 	// Constructor
@@ -35,151 +35,151 @@ public class StudentManager {
 		this.studentList = studentList;
 	}
 
-	// Check if student ID is valid
-	public boolean studentIdFormatValidator(String studentId) {
-		// Check if student ID is valid
-		if (studentId == null) {
-			System.err.println("Student NOT added! - Student ID can not be null");
-			return false;
-		} else if (!(studentId.matches("G00\\d{6}"))) {
-			System.err.println("Student NOT added! - Student ID must match the format G00123456");
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	// Returns true if duplicate is detected and false if not
-	public boolean studentDuplicateDetector(Student student) {
-		if (student == null) {
-			System.err.println("Input can not be null!");
-			return false;
-		}
-		// Check if a valid student ID was entered
-		if (studentIdFormatValidator(student.getStudentId())) {
-			for (Student studentObject : studentList) {
-				if (student.getStudentId().equals(studentObject.getStudentId())) {
-					System.out.println("Student ID " + student.getStudentId() + " already on List");
-					return true;
-				}
+	// Find student object by ID. Returns null if student is not found.
+	public Student findStudentObjectByID(String studentId) {
+		// Search all student objects in the student list
+		for (Student studentObject : studentList) {
+			// Compare IDs to ID passed in
+			if (studentId.equals(studentObject.getStudentId())) {
+				// If a match is found return the student object
+				System.out.println("Student object with ID = " + studentId + " was found on list!");
+				return studentObject;
 			}
-			return false;
 		}
-		return false;
-	}
-
-	/* Returns true if duplicate is detected and false if not. */
-	public boolean studentDuplicateDetector(String studentId) {
-		// Check if a valid student ID was entered
-		if (studentIdFormatValidator(studentId)) {
-			for (Student studentObject : studentList) {
-				if (studentId.equals(studentObject.getStudentId())) {
-					System.err.println("Student ID " + studentId + " already on List");
-					return true;
-				}
-			}
-			return false;
-		}
-		return false;
-	}
-
-	// Find student by ID
-	public Student findStudentByID(String studentId) {
-		// Check if a valid student ID was entered
-		if (studentIdFormatValidator(studentId)) {
-			for (Student studentObject : studentList) {
-				if (studentId.equals(studentObject.getStudentId())) {
-					System.out.println("Student with ID " + studentId + " was found!");
-					return studentObject;
-				} // End if
-			} // End for
-			System.err.println("Student with ID " + studentId + " was NOT found!");
-			return null;
-		}
+		// If no match is found return null
+		System.out.println("Student object with ID = " + studentId + " was NOT found on list!");
 		return null;
 	}
 
+	// Returns true if student on list
+	public boolean isOnList(String studentId) {
+		return studentList.contains(findStudentObjectByID(studentId));
+	}
+
+	public void findStudentsByName(String firstName) {
+		System.out.println("Here are all students with first name " + firstName + ":");
+		// Search all student objects in the student list
+		for (Student studentObject : studentList) {
+			// Compare IDs to ID passed in
+			if (firstName.equals(studentObject.getFirstName())) {
+				// If a match is found print the student details to console
+				System.out.println(studentObject.toString());
+			}
+		}
+	}
+
+	public void findStudentsByAge(int age) {
+		System.out.println("Here are all students of age " + age + ":");
+		// Search all student objects in the student list
+		for (Student studentObject : studentList) {
+			// Compare IDs to ID passed in
+			if (age == studentObject.getAge()) {
+				// If a match is found print the student details to console
+				System.out.println(studentObject.toString());
+			}
+		}
+	}
+
+	public void findStudentsByFirstNameAndAge(String firstName, int age) {
+		System.out.println("Here are all students with first name " + firstName + " and age " + age + ":");
+		// Search all student objects in the student list
+		for (Student studentObject : studentList) {
+			// Compare IDs to ID passed in
+			if (studentObject.getFirstName().equals(firstName) && studentObject.getAge() == age) {
+				// If a match is found print the student details to console
+				System.out.println(studentObject.toString());
+			}
+		}
+	}
+
+	public Student findStudentsByAgeRange(int minAge, int maxAge) {
+		if (minAge > maxAge) {
+			System.err.println("Invalid input: Minimum age can not be greater than maximum age!");
+			return null;
+		} else {
+			System.out.println("Here are all students between the ages of " + minAge + " and " + maxAge + ":");
+			// Search all student objects in the student list
+			for (Student studentObject : studentList) {
+				// Compare IDs to ID passed in
+				if (studentObject.getAge() >= minAge && studentObject.getAge() <= maxAge) {
+					// If a match is found print the student details to console
+					System.out.println(studentObject.toString());
+				}
+			}
+			// If no match is found return null
+			return null;
+		}
+	}
+
 	// Add student to list
-	public boolean addStudentToList(Student newStudent) {
-		if (studentIdFormatValidator(newStudent.getStudentId())) {
-			if (studentDuplicateDetector(newStudent.getStudentId())) {
-				return false;
-			} else {
-				// ID format good and no duplicate found. Adding student to the list
-				return this.studentList.add(newStudent);
-			}
-		}
-		return false; // If studentIdFormatValidator detects mis formatted id
-	}
-
 	public boolean addStudentToList(String studentId, String name, int age) {
-		Student newStudent = new Student(studentId, name, age);
-		// Check if a valid student ID was entered
-		if (studentIdFormatValidator(newStudent.getStudentId())) {
-			if (studentDuplicateDetector(newStudent.getStudentId())) {
-				return false;
-			} else {
-				// ID format good and no duplicate found. Adding student to the list
-				return this.studentList.add(newStudent);
-			}
+		// Check student details are vaild and if student is NOT already on list
+		if (Student.isValid(studentId, name, age) && !isOnList(studentId)) {
+			// Create student object with valid details and add student to the list
+			Student newStudent = new Student(studentId, name, age);
+			return this.studentList.add(newStudent);
 		}
-		return false; // If studentIdFormatValidator detects mis formatted id
-	}
-
-	// Remove Student from list
-	public boolean removeStudentFromList(Student newStudent) {
-		return this.studentList.remove(newStudent);
+		// If student details are invalid or if student is already on list return false
+		System.out.println("Student with ID" + studentId + " could not be added to list!");
+		return false;
 	}
 
 	// Remove student from list given studendID
 	public boolean removeStudentFromList(String studentId) {
-		if (studentIdFormatValidator(studentId)) {
-			for (Student studentObject : studentList) {
-				if (studentId.equals(studentObject.getStudentId())) {
-					return studentList.remove(studentObject);
-				} // End if
-			} // End for
-		}
-		return false;
+		return studentList.remove(findStudentObjectByID(studentId));
 	}
 
 	// Update student name
 	public boolean updateStudentName(String studentId, String newName) {
-		Student studentObject = findStudentByID(studentId);
-		if (studentObject != null) {
-			studentObject.setName(newName);
-			System.out.println("Student name updated!");
-			return true;
+		// If the inputted Student ID and new name are valid...
+		if (Student.studentIdIsValid(studentId) && Student.firstNameIsValid(newName)) {
+			// Search for student object with on the list with the inputted ID
+			Student studentToUpdate = findStudentObjectByID(studentId);
+			// If the student object is found i.e. NOT equal to null...
+			if (studentToUpdate != null) {
+				// Save the current students name
+				String currentName = studentToUpdate.getFirstName();
+				// Check if the currentName is equal to the newName
+				if (currentName.equals(newName)) {
+					System.out.println("Student name is already " + newName + "! No update made to name.");
+				} else {
+					// else if the currentName is NOT equal to the newName, update the name
+					studentToUpdate.setFirstName(newName);
+					System.out.println("Student name changed from " + currentName + " to " + newName + "!");
+					return true;
+				}
+			}
 		}
-		System.err.println("Student name NOT updated!");
+		// If student ID is invalid or new name is invalid
+		System.out.println("Student name NOT updated!");
 		return false;
 	}
 
 	// Show total number of Students in List
-	public void showTotalStudents() {
+	public void printTotalNumberOfStudents() {
 		System.out.println(this.studentList.size());
 	}
 
-	// Show total number of Students in List
-	public int totalStudents() {
-		return this.studentList.size();
-	}
-
-	// Print all student details in table
-	public void printAllStudentDetails() {
+	// Print student list to console
+	public void printStudentList() {
 		System.out.println("ID,NAME,AGE");
+		System.out.println("========================");
 		for (Student studentObject : studentList) {
 			System.out.println(studentObject.toString());
 		}
+		System.out.println("========================");
 	}
 
-	public void loadStudentsFromCSVFile(File studentCSVFile) {
+	// Read student details from file
+	public void readStudentDataFromCSVFile(String pathToStudentCSVFile) {
+		File studentCSVFile = null;
 		FileReader studentCSVFileReader = null;
 		BufferedReader bufferedStudentCSVFileReader = null;
 		String bufferData = null; // Used to store lines of data we read from the buffer
 
 		// Create a file reader
 		try {
+			studentCSVFile = new File(pathToStudentCSVFile);
 			studentCSVFileReader = new FileReader(studentCSVFile);
 			// Add a buffer to the file reader
 			bufferedStudentCSVFileReader = new BufferedReader(studentCSVFileReader);
@@ -190,62 +190,76 @@ public class StudentManager {
 				// System.out.println(bufferData);
 				String[] studentFieldValues = bufferData.split(",");
 				// System.out.println(Arrays.toString(studentFieldValues));
-				Student newStudent = new Student(studentFieldValues[0], studentFieldValues[1],
-						Integer.parseInt(studentFieldValues[2]));
-				this.addStudentToList(newStudent); // Add student to the studentList
+				String studentId = studentFieldValues[0];
+				String firstName = studentFieldValues[1];
+				int age = Integer.parseInt(studentFieldValues[2]);
+				this.addStudentToList(studentId, firstName, age); // Add student to the studentList
 			}
-			System.out.println("Loaded Students List from CSV file successfully!");
+			System.out.println("Student data read from CSV file located at " + pathToStudentCSVFile);
+		} catch (NullPointerException npExc) {
+			System.err.println("ERROR: Students NOT saved to file!");
+			npExc.printStackTrace();
 		} catch (FileNotFoundException fnfExc) {
+			System.err.println("ERROR: Students NOT saved to file!");
 			fnfExc.printStackTrace();
 		} catch (IOException IOExc) {
+			System.err.println("ERROR: Students NOT saved to file!");
 			IOExc.printStackTrace();
 		} finally {
 			try {
-				if (studentCSVFileReader != null) {
-					// Flushes buffer, which transfers buffer data to the file, then closes buffer.
-					bufferedStudentCSVFileReader.close();
-					// Close the file reader stream
-					studentCSVFileReader.close();
-				}
+				// Flushes buffer, which transfers buffer data to the file, then closes buffer.
+				bufferedStudentCSVFileReader.close();
+				// Close the file reader stream
+				studentCSVFileReader.close();
 			} catch (IOException IOExc) {
+				System.err.println("ERROR: Could not close the buffer file reader!");
 				IOExc.printStackTrace();
 			} // End catch
 		} // End finally
-	} // End load method
+	} // End read method
 
-	public void saveStudentsToCSVFile(File studentDBFile) {
+	// Write student details to file
+	public void writeStudentDataToCSVFile(String pathToStudentCSVFile) {
+		File studentCSVFile = null;
 		FileWriter studentFileWriterStream = null;
 		BufferedWriter bufferedstudentFileWriterStream = null;
 		try {
-			studentFileWriterStream = new FileWriter(studentDBFile);
+			// Create a buffered file writer which can write one line at a time
+			studentCSVFile = new File(pathToStudentCSVFile);
+			studentFileWriterStream = new FileWriter(studentCSVFile);
 			bufferedstudentFileWriterStream = new BufferedWriter(studentFileWriterStream);
+
+			// Write column headers to CSV file
 			bufferedstudentFileWriterStream.write("ID,Firstname,Age" + "\n");
 
 			// Write out student data from studentList to buffer and flush it to CSV file
 			for (Student studentObject : studentList) {
-				bufferedstudentFileWriterStream.write(studentObject.getStudentId() + "," + studentObject.getFirstname()
-						 + "," + studentObject.getAge() + "\n");
-				// bufferedstudentFileWriterStream.write(studentObject.findAllFieldValuesInCSVFormat()
-				// + "\n");
-				bufferedstudentFileWriterStream.flush(); // Flushes buffer which transfers buffer data to the file
+				bufferedstudentFileWriterStream.write(studentObject.getStudentId() + "," + studentObject.getFirstName()
+						+ "," + studentObject.getAge() + "\n");
+				bufferedstudentFileWriterStream.flush(); // Flush buffer and transfer buffer data to the file
 			}
-			System.out.println("Saved Students List to CSV file successfully!");
+			System.out.println("Student data written to CSV file located at " + pathToStudentCSVFile);
+		} catch (NullPointerException npExc) {
+			System.err.println("ERROR: Students NOT saved to file!");
+			npExc.printStackTrace();
 		} catch (FileNotFoundException fnfExc) {
+			System.err.println("ERROR: Students NOT saved to file!");
 			fnfExc.printStackTrace();
 		} catch (IOException IOExc) {
+			System.err.println("ERROR: Students NOT saved to file!");
 			IOExc.printStackTrace();
 		} finally {
 			try {
-				if (studentFileWriterStream != null) {
-					// Close buffer
-					bufferedstudentFileWriterStream.close();
-					// Close file writer
-					studentFileWriterStream.close();
-				}
-			} catch (IOException IOExc) {
-				IOExc.printStackTrace();
-			} // End catch
+				// Close buffer
+				bufferedstudentFileWriterStream.close();
+				// Close file writer
+				studentFileWriterStream.close();
+			} catch (NullPointerException npExc) {
+				System.out.println("ERROR: Could not close the ObjectOutputStream or FileOutputStream!");
+				npExc.printStackTrace();
+			} catch (IOException ioExc) {
+				ioExc.printStackTrace();
+			} // End catchEnd catch
 		} // End finally
-	} // End Save method
-
+	}
 } // End Class
